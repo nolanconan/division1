fa650982f595b60cb44b47c71d64949306ef47f2
alexisalou <salou.a@stjolorient.fr>
2025-01-27 16:20:56 +0100

    ajout serveur

--- UDP/serveur_udp_division.c
+++ UDP/serveur_udp_division.c
@@ -6,13 +6,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
-
+int traiter_commande_wrapper(int argc, char** argv){
+	traiter_commande(argc==2, argv[0], "<port>\nmauvais nombre d'arguments");
+	traiter_commande (est_un_port_non_reserve(argv[2]), argv[0], "<port>\n<port> est un port non réservé");
+}
 int main(int argc, char** argv) {
-	traiter_commande(/*A COMPLETER*/, argv[0], "<port>\nmauvais nombre d'arguments");
-	traiter_commande(/*A COMPLETER*/, argv[0], "<port>\n<port> est un port non réservé");
-
-	/*A COMPLETER*/
-
+	traiter_commande_wrapper(argc,argv);{}
+	
 	exit(0);
 }
+
 
