ce14e462fb936c4a57f23e9d74efd78f5471e3d2
nolanconan <conan.n@stjolorient.fr>
2025-01-27 16:26:28 +0100

    ajout du fichier serveur_udp_division.c

--- UDP/serveur_udp_division.c
+++ UDP/serveur_udp_division.c
@@ -12,7 +12,7 @@ int traiter_commande_wrapper(int argc, char** argv){
 }
 int main(int argc, char** argv) {
       
-      traiter_commande_wrapper(argc,argv);
+      traiter_commande_wrapper(argc,argv);{}
       
 
 
