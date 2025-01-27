a4c6b173a2babdebc3df29434a1f174519ce847c
alexisalou <salou.a@stjolorient.fr>
2025-01-27 16:07:36 +0100

    fin UDP

--- UDP/udp.c
+++ UDP/udp.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
@@ -28,11 +29,13 @@ void creer_socket(char* adresseIP, int port, SOCK* sock) {
 
 /* Attacher une socket */
 void attacher_socket(SOCK* sock) {
-	bind()
+	
+	bind(sock->sockfd,(struct sockaddr *)&sock->adresse, sizeof (sock->adresse));
+	
 	traiter_erreur(__FUNCTION__);
 }
 
-/*Initialiser la structure adresse */
+/*Initialiser la structure adresse client */
 void init_addr(SOCK* sock) {
 	memset(&sock->adresse, 0, sizeof(sock->adresse));
 }
@@ -45,13 +48,13 @@ void dimensionner_file_attente_socket(int taille, SOCK* sock) {
 
 /* Recevoir un message */
 void recevoir_message(SOCK* dst, char * buffer) {
-	receive()
+	recvfrom(dst->sockfd, buffer, sizeof(buffer), 0,(struct sockaddr *)&dst->adresse,&dst->longueur_adresse);
 	traiter_erreur(__FUNCTION__);
 }
 
 /* Émettre un message */
 void envoyer_message(SOCK* dst, char * message) {
-	send()
+	sendto(dst->sockfd, message, strlen(message), 0,(struct sockaddr *)&dst->adresse, sizeof(dst->adresse));
 	traiter_erreur(__FUNCTION__);
 }
 /* Fermer la connexion */
