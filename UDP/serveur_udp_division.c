8ce67a159fe9b48d85a45888f299353e867e2374
alexisalou <salou.a@stjolorient.fr>
2025-02-03 13:39:39 +0100

    changement serveur UDP

--- UDP/serveur_udp_division.c
+++ UDP/serveur_udp_division.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <netinet/in.h>
 int traiter_commande_wrapper(int argc, char** argv){
-      traiter_commande(argc[2], argv[0], "<port>\nmauvais nombre d'arguments");
+      traiter_commande(argc==2, argv[0], "<port>\nmauvais nombre d'arguments");
       traiter_commande (est_un_port_non_reserve(argv[2]), argv[0], "<port>\n<port> est un port non réservé");
 }
 int main(int argc, char** argv) {
