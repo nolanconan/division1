8f7fec1c2a8ac27a3ffd65d24c4ac0780e190107
alexisalou <salou.a@stjolorient.fr>
2025-02-03 14:16:28 +0100

    modification serveur_udp_division.c

--- UDP/serveur_udp_division.c
+++ UDP/serveur_udp_division.c
@@ -3,34 +3,42 @@
 #include "nombre.h"
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <netinet/in.h>
 
 int main(int argc, char** argv) {
+    // Vérification du nombre d'arguments
     traiter_commande(argc == 2, argv[0], "<port>\nmauvais nombre d'arguments");
 
+    // Vérification si le port est non réservé
     traiter_commande(est_un_port_non_reserve(argv[1]), argv[0], "<port>\n<port> est un port non réservé");
 
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        traiter_erreur("Erreur lors de la création de la socket");
+    // Conversion du port en entier
+    int port = (int) convertir_nombre(argv[1]);
+
+    // Création et attachement de la socket
+    SOCK sock;
+    creer_socket("", port, &sock);
+    attacher_socket(&sock);
+
+    // Buffer pour recevoir les messages
+    char buffer[1024];
+
+    printf("Serveur en attente de messages sur le port %d...\n", port);
+
+    while (1) {
+        // Réception d'un message
+        recevoir_message(&sock, buffer);
+        printf("Message reçu : %s\n", buffer);
+
+        // Émission d'une réponse
+        char* reponse = "Message reçu";
+        envoyer_message(&sock, reponse);
     }
 
-    // Configuration de l'adresse du serveur
-    struct sockaddr_in servaddr;
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons((uint16_t) convertir_nombre(argv[1]));
+    // Fermeture de la connexion
+    fermer_connexion(&sock);
 
-    // Liaison de la socket avec l'adresse du serveur
-    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        traiter_erreur("Erreur lors de la liaison de la socket");
-    }
-
-    // Ajoutez ici le code pour recevoir et traiter les messages UDP
-
-    fermer_connexion(sockfd);
     exit(0);
 }
